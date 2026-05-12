eee7faa6e80b576b0e564133e7f9631f9ecaaefa
Rejeesh R <rrejeesh12@gmail.com>
2026-05-12 23:08:33 +0530

    Fix case-sensitive model import

--- backend/routes/propertyRoutes.js
+++ backend/routes/propertyRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Property = require("../models/Property");
+const Property = require("../models/property");
 
 // CREATE
 router.post("/", async (req, res) => {
